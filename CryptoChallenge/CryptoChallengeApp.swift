10af8fbfa0b94a500914b6d260c6657c7f5c9913
Pablo Navarro Zepeda <88807082+panaze@users.noreply.github.com>
2025-01-18 20:42:51 -0600

    Initial Commit

---
+++ CryptoChallenge/CryptoChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoChallengeApp.swift
+//  CryptoChallenge
+//
+//  Created by Pablo Navarro Zepeda on 18/01/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
